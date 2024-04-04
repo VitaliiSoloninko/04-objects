251ae26c165311e21c6a28a41bf7a11a6b504fe7
22051988Di <vitalii.soloninko@gmail.com>
2024-04-04 10:50:04 +0200

    Context this

--- 04-objects.js
+++ 04-objects.js
@@ -1,6 +1,6 @@
 const object = {}
-console.log(typeof object) // тип обект
-console.log(typeof null) // помилково теж тип обект
+// console.log(typeof object) // тип обект
+// console.log(typeof null) // помилково теж тип обект
 
 // ====================== Objects
 
@@ -8,7 +8,7 @@ const person = {
 	name: 'Vitalii', // key: 'meaning' ключ: 'значення'
 	age: 36,
 	isYoutuber: false,
-	languages: ['ua', 'ru', 'de', 'en'],
+	languages: ['ua', 'ru', 'en'],
 	address: {
 		city: 'Rivne',
 		street: 'Soborna',
@@ -33,7 +33,63 @@ const person = {
 		console.log('Person name', this.name)
 	},
 }
-console.log(person)
-person.greet()
-person.arrow()
-person.info()
+// console.log(person)
+// person.greet()
+// person.arrow()
+// person.info()
+
+// Як звертатися до даних з обекту
+// console.log(person.address)
+// console.log(person['address'])
+// console.log(person['complex key'])
+
+// динамічний ключ
+// const addressKey = 'address'
+// console.log(person[addressKey])
+
+// Змінювати дані обекта
+// person.age++ // зміна
+// person.languages.push('de') // додавання
+// console.log(person.age)
+// console.log(person.languages)
+
+// person.address = undefined // приховування, не правильно
+// delete person.address // видалення
+// console.log(person)
+
+// Деструктуризація, в окрему змінну отримати тільки частину обєкту
+// Наприклад імя вік мови
+
+// -------------- Destructuring
+// const name = person.name
+// const age = person.age
+// const languages = person.languages
+
+// const name = 'Petr'
+
+// const { age, name: firstName = 'Test', languages } = person
+// // name - звідки беремо firstName - нова змінна 'Test' - стандартне значення
+
+// console.log(languages, age, firstName)
+
+// ----------- Ітерація по ключах обекту
+
+// 1 варінат
+// якщо використовувати цикл в обєктах, то часто може бути помилка
+// for (let key in person) {
+// 	// if перевірка, щоб не заходив в глобальний прототип
+// 	// питають на співбесіді
+// 	if (person.hasOwnProperty(key)) {
+// 	}
+// 	console.log(person[key])
+// }
+
+// 2 варінат
+
+// console.dir(Object) // щоб побачити методи
+// на виході ми отримаюмо масив з ключів
+// Object.keys(person).forEach(key => {
+// 	console.log(person[key])
+// })
+
+// Контекст Context this
